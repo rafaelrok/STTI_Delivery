32cbe2068011ba5a875650b45902bf019f6d7d46
Rafaelrok <rafaelvieirasantos2006@hotmail.com>
2021-01-08 01:59:39 -0300

    Add Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
+import Home from './Home';
 import Navbar from './Navbar';
 
 function App() {
   return (
-    <div >
+    <div>
       <Navbar />
+      <Home />
     </div>
   );
 }
